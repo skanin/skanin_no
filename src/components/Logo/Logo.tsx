d3a0c45a3517689e4a9f8285fbf696f8817e73e3
Marius Christopher Sjøberg <mariussjoberg@Mariuss-MacBook-Pro.local>
2020-10-22 22:52:24 +0200

    simple styling on the logo 💰

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { ReactComponent as ReactLogo } from '../../assets/img/SVG/logo.svg';
 
 const Logo: React.FunctionComponent = () => {
-	return <ReactLogo></ReactLogo>;
+	return (
+		<div id='splashart'>
+			<ReactLogo />;
+		</div>
+	);
 };
 
 export default Logo;
